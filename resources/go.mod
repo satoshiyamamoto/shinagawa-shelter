3ca75ee7f6d59b0d79e24e6aa0b0683334a8df72
satoshi_yamamoto <satoshi_yamamoto@cyberagent.co.jp>
2022-12-29 13:03:15 +0900

    feat(model): Added constructor for CSV

--- resources/go.mod
+++ resources/go.mod
@@ -1,8 +1,10 @@
-require github.com/aws/aws-lambda-go v1.23.0
+require (
+	github.com/aws/aws-lambda-go v1.23.0
+	golang.org/x/text v0.5.0 // indirect
+)
 
 replace gopkg.in/yaml.v2 => gopkg.in/yaml.v2 v2.2.8
 
 module shinagawa-shelter
 
 go 1.16
-
--- resources/pkg/model/model.go
+++ resources/pkg/model/model.go
@@ -1,6 +1,39 @@
 package model
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
+
+const (
+	facilityName = iota
+	englishFacilityName
+	category
+	prefecture
+	city
+	administrativeDistrict
+	address
+	addressKana
+	telephoneNumber
+	targetDistrict
+	targetDistrictDescription
+	latitude
+	longitude
+	aboveSeaLevel
+	facilityCapacity
+	facilityHeight
+	flood
+	landslide
+	stormSurge
+	earthquake
+	tsunami
+	inundation
+	fire
+	eruption
+	description
+	englishDescription
+	standardAreaCode
+)
 
 type Shelter struct {
 	ID                        int       `json:"id"`
@@ -33,4 +66,60 @@ type Shelter struct {
 	StandardAreaCode          string    `json:"standard_area_code"`
 	CreatedAt                 time.Time `json:"created_at"`
 	UpdatedAt                 time.Time `json:"updated_at"`
+}
+
+func floats(s string, def float64) float64 {
+	if len(s) == 0 {
+		return def
+	}
+
+	f, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return def
+	}
+
+	return f
+}
+
+func ints(s string, def int) int {
+	return int(floats(s, 0))
+}
+
+func bools(s string, def bool) bool {
+	if len(s) == 0 {
+		return def
+	}
+	return true
+}
+
+func NewShelter(record []string) *Shelter {
+	return &Shelter{
+		FacilityName:              record[facilityName],
+		EnglishFacilityName:       record[englishFacilityName],
+		Category:                  record[category],
+		Prefecture:                record[prefecture],
+		City:                      record[city],
+		AdministrativeDistrict:    record[administrativeDistrict],
+		Address:                   record[address],
+		AddressKana:               record[addressKana],
+		TelephoneNumber:           record[telephoneNumber],
+		TargetDistrict:            record[targetDistrict],
+		TargetDistrictDescription: record[targetDistrictDescription],
+		Latitude:                  floats(record[latitude], 0),
+		Longitude:                 floats(record[longitude], 0),
+		AboveSeaLevel:             floats(record[aboveSeaLevel], 0),
+		FacilityCapacity:          ints(record[facilityCapacity], 0),
+		FacilityHeight:            floats(record[facilityHeight], 0),
+		Flood:                     bools(record[flood], false),
+		Landslide:                 bools(record[landslide], false),
+		StormSurge:                bools(record[stormSurge], false),
+		Earthquake:                bools(record[earthquake], false),
+		Tsunami:                   bools(record[tsunami], false),
+		Inundation:                bools(record[inundation], false),
+		Fire:                      bools(record[fire], false),
+		Eruption:                  bools(record[eruption], false),
+		Description:               record[description],
+		EnglishDescription:        record[englishDescription],
+		StandardAreaCode:          record[standardAreaCode],
+	}
 }
